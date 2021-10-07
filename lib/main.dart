5527ce3c9084bba9055d512b4913017107acb0c8
Arman Arif <arman3472rmn@gmail.com>
2021-10-08 03:36:38 +0600

    Separated Logics design into deferent files

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Easy List'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advance Food Court');
-                  });
-                },
-                child: Text('Add Product'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map((element) => Card(
-                        margin: EdgeInsets.all(15.0),
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/images/table_with_food.jpg'),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(element),
-                            ),
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            ),
-          ],
-        ),
+        body: ProductManager(),
       ),
     );
   }
