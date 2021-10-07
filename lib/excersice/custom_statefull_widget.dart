cae6f40bca0123aed0ac7e0088a35e4b383d1a7d
Arman Arif <arman3472rmn@gmail.com>
2021-10-08 03:19:12 +0600

    Added excersice to write stateless and statefull widget from scrach

---
+++ lib/excersice/custom_statefull_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  @override
+  State createState() {
+    return _MyWidgetState();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
